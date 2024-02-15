50aa3d3eed0311e184df9a8675a7131caebe424d
Philippe Mathieu <philippe.mathieu@univ-lille.fr>
2024-02-15 16:32:34 +0100

    changes for jakarta jstl

--- src/main/java/fr/da2i/jpo/controller/SaisieController.java
+++ src/main/java/fr/da2i/jpo/controller/SaisieController.java
@@ -6,16 +6,13 @@ import jakarta.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import ch.qos.logback.core.joran.conditional.ElseAction;
 import fr.da2i.jpo.entities.Lycee;
 import fr.da2i.jpo.entities.Visiteur;
 import fr.da2i.jpo.repositories.LyceeRepository;
