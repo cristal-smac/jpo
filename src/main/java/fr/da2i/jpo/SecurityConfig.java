4aa6057c63b57eced2335fb434441d9dc06470b6
Nathan Hallez <nathan.hallez.etu@univ-lille.fr>
2024-05-08 11:16:59 +0200

    Added Spring Security for /all endpoint

---
+++ src/main/java/fr/da2i/jpo/SecurityConfig.java
@@ -0,0 +1,23 @@
+package fr.da2i.jpo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/all").authenticated()
+                        .anyRequest().permitAll())
+                .httpBasic(withDefaults());
+        return httpSecurity.build();
+    }
+}
