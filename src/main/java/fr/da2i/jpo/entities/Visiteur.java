0f57574ff98bce2749155b76d2a9cc8c81381afd
Philippe Mathieu <philippe.mathieu@univ-lille.fr>
2025-01-23 22:32:54 +0100

    suppression de lignes inutiles

--- src/main/java/fr/da2i/jpo/entities/Visiteur.java
+++ src/main/java/fr/da2i/jpo/entities/Visiteur.java
@@ -8,9 +8,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Entity
